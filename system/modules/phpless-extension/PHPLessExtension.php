db943653154e7f27dbae768de10d7d025f9d1de4
Tristan Lins <tristan.lins@infinitysoft.de>
2011-07-20 14:23:40 +0200

    Improve all functions.

--- system/modules/phpless-extension/PHPLessExtension.php
+++ system/modules/phpless-extension/PHPLessExtension.php
@@ -47,7 +47,10 @@ class PHPLessExtension
 			{
 				$out = '#';
 				
-				foreach (array(4, 1, 2, 3) as $i)
+				// the alpha value
+				$out .= strtoupper(($value[$i] < 16 ? '0' : '').dechex(255*$value[4]));
+				// the rgb values
+				foreach (array(1, 2, 3) as $i)
 				{
 					$out .= strtoupper(($value[$i] < 16 ? '0' : '').dechex($value[$i]));
 				}
@@ -65,6 +68,36 @@ class PHPLessExtension
 		{
 			$c->append($strPrefix . $key, $value);
 		}
+	}
+	
+	protected function prependTags($prefix, $rtags)
+	{
+		if (!is_array($prefix))
+		{
+			$prefix = array($prefix);
+		}
+		
+		$tags = array();
+		foreach ($prefix as $pre)
+		{
+			list($pre_element, $pre_class) = explode('.', $pre, 2);
+			foreach ($rtags as $tag)
+			{
+				$path = explode(' ', $tag);
+				list($tag_element, $tag_class) = explode('.', $path[0], 2);
+				if ($pre_element == $tag_element)
+				{
+					$path[0] = $pre_element . '.' . $pre_class . '.' . $tag_class;
+				}
+				else
+				{
+					array_unshift($path, $pre);
+				}
+				$tags[] = implode(' ', $path);
+			}
+		}
+		
+		return $tags;
 	}
 	
 	public function hookLesscssHandleProperty($key, $value, lessc $c)
@@ -94,15 +127,23 @@ class PHPLessExtension
 			return true;
 			
 		case 'gradient':
+		case 'hgradient':
+			$tags = $c->multiplyTags();
+			
 			if ($argv[0] == 'list')
 			{
 				$argv = $argv[2];
 			}
 			
+			$images = array();
 			$colors = array();
 			$n = 0;
 			while ($n < count($argv))
 			{
+				if ($argv[$n][0] == 'function')
+				{
+					$argv[$n] = $c->funcToColor($argv[$n]);
+				}
 				if ($argv[$n][0] == 'color')
 				{
 					$color = $argv[$n++];
@@ -120,51 +161,152 @@ class PHPLessExtension
 				}
 				else
 				{
-					return '';
+					$images[] = $c->compileValue($argv[$n++]);
 				}
 			}
 			
 			$n = count($colors);
+			$m = 1 / ($n-1);
 			
 			if ($n < 2)
 			{
 				return '';
 			}
 			
+			$return = '';
+			
 			// webkit gradients
-			$out = '-webkit-gradient(linear, left top, left bottom';
+			$out = '';
+			foreach ($images as $image)
+			{
+				$out .= $image . ', ';
+			}
+			$out .= '-webkit-gradient(linear, ';
+			switch ($key) {
+			case 'gradient':
+				$out .= 'left top, left bottom';
+				break;
+			case 'hgradient':
+				$out .= 'left top, right top';
+				break;
+			}
 			foreach ($colors as $i => $color)
 			{
-				$out .= sprintf(', color-stop(%f, %s)', $color[1] >= 0 ? $color[1] : $n / $i, $c->compileValue($color[0]));
+				$out .= sprintf(', color-stop(%f, %s)', $color[1] >= 0 ? $color[1] : $i * $m, $c->compileValue($color[0]));
 			}
 			$out .= ')';
-			$c->append('background', array('keyword', $out));
+			$return .= $c->compileBlock($this->prependTags(array('body.safari', 'body.chrome'), $tags), array('background' => array(array('keyword', $out))));
 			
 			// mozilla gradients
-			$out = '-moz-linear-gradient(center top';
+			$out = '';
+			foreach ($images as $image)
+			{
+				$out .= $image . ', ';
+			}
+			$out .= '-moz-linear-gradient(center ';
+			switch ($key) {
+			case 'gradient':
+				$out .= 'top';
+				break;
+			case 'hgradient':
+				$out .= 'left';
+				break;
+			}
 			foreach ($colors as $i => $color)
 			{
-				$out .= sprintf(', %s %d%%', $c->compileValue($color[0]), ($color[1] >= 0 ? $color[1] : $n / $i) * 100);
+				$out .= sprintf(', %s %d%%', $c->compileValue($color[0]), ($color[1] >= 0 ? $color[1] : $i * $m) * 100);
 			}
 			$out .= ')';
-			$c->append('background', array('keyword', $out));
+			$return .= $c->compileBlock($this->prependTags('body.firefox', $tags), array('background' => array(array('keyword', $out))));
 			
 			// opera gradients
-			$out = '-o-linear-gradient(center top';
+			$out = '';
+			foreach ($images as $image)
+			{
+				$out .= $image . ', ';
+			}
+			$out .= '-o-linear-gradient(center ';
+			switch ($key) {
+			case 'gradient':
+				$out .= 'top';
+				break;
+			case 'hgradient':
+				$out .= 'left';
+				break;
+			}
 			foreach ($colors as $i => $color)
 			{
-				$out .= sprintf(', %s %d%%', $c->compileValue($color[0]), ($color[1] >= 0 ? $color[1] : $n / $i) * 100);
+				$out .= sprintf(', %s %d%s', $c->compileValue($color[0]), ($color[1] >= 0 ? $color[1] : $i * $m) * 100, '%');
 			}
 			$out .= ')';
-			$c->append('background', array('keyword', $out));
+			$return .= $c->compileBlock($this->prependTags('body.opera', $tags), array('background' => array(array('keyword', $out))));
 			
 			// ms filter
-			$out = 'progid:DXImageTransform.Microsoft.Gradient(enabled=true, StartColorStr=' . $this->parseARGB($colors[0][0], $c) . ', EndColorStr=' . $this->parseARGB($colors[$n-1][0], $c) . ')';
-			$c->append('filter', array('keyword', $out));
-			$out = 'progid:DXImageTransform.Microsoft.Gradient(enabled=true, StartColorStr=' . $this->parseARGB($colors[0][0], $c) . ', EndColorStr=' . $this->parseARGB($colors[$n-1][0], $c) . ')';
-			$c->append('-ms-filter', array('keyword', $out));
+			$out = 'progid:DXImageTransform.Microsoft.Gradient(enabled=true' . ($key == 'hgradient' ? ', GradientType=1' : '') . ', StartColorStr=' . $this->parseARGB($colors[0][0], $c) . ', EndColorStr=' . $this->parseARGB($colors[$n-1][0], $c) . ')';
+			$return .= $c->compileBlock($this->prependTags(array('body.ie6', 'body.ie7'), $tags), array('filter' => array(array('keyword', $out))));
+			$return .= $c->compileBlock($this->prependTags('body.ie8', $tags), array('-ms-filter' => array(array('keyword', $out))));
 			
-			return true;
+			// IE9 svg gradient
+			$out = '<?xml version="1.0" ?>
+<svg xmlns="http://www.w3.org/2000/svg" preserveAspectRatio="none" version="1.0" width="100%" height="100%" xmlns:xlink="http://www.w3.org/1999/xlink">
+	<defs>';
+			switch ($key) {
+			case 'gradient':
+				$out .= '
+		<linearGradient id="myLinearGradient1" x1="0%" y1="0%" x2="0%" y2="100%" spreadMethod="pad">';
+				break;
+			case 'hgradient':
+				$out .= '
+		<linearGradient id="myLinearGradient1" x1="0%" y1="0%" x2="100%" y2="0%" spreadMethod="pad">';
+				break;
+			}
+			foreach ($colors as $i => $color)
+			{
+				$opacity = 1;
+				if (count($color[0]) == 5)
+				{
+					$opacity = $color[0][4];
+					unset($color[0][4]);
+				}
+				$out .= sprintf('
+			<stop offset="%d%%" stop-color="%s" stop-opacity="%d"/>',
+					($color[1] >= 0 ? $color[1] : $i * $m) * 100,
+					$c->compileValue($color[0]),
+					$opacity);
+			}
+			$out .= '
+		</linearGradient>
+	</defs>
+	<rect width="100%" height="100%" style="fill:url(#myLinearGradient1);" />
+</svg>';
+			$strSVG = 'system/html/ie9_gradient_' . substr(md5($out), 0, 8) . '.svg';
+			if (!file_exists(TL_ROOT . '/' . $strSVG))
+			{
+				$objFile = new File($strSVG);
+				$objFile->write($out);
+				$objFile->close();
+			}
+			
+			// ms background
+			if (count($images))
+			{
+				$return .= $c->compileBlock($this->prependTags(array('body.ie6', 'body.ie7', 'body.ie8'), $tags), array('background' => array(array('keyword', $images[0]))));
+				
+				$out = '';
+				foreach ($images as $image)
+				{
+					$out .= $image . ', ';
+				}
+				$out .= 'url(../../' . $strSVG . ')';
+				$return .= $c->compileBlock($this->prependTags('body.ie9', $tags), array('background' => array(array('keyword', $out))));
+			}
+			else
+			{
+				$out = 'url(../../' . $strSVG . ')';
+				$return .= $c->compileBlock($this->prependTags('body.ie9', $tags), array('background' => array(array('keyword', $out))));
+			}
+			
+			return $return;
 		}
 		return false;
 	}
